4d8c08c742d32258038e4874bb2b64345c1c9b60
tiniakov <tiniakov79@gmail.com>
2024-09-29 22:58:45 +0300

    add replics

--- mongo-sharding-repl/init.sh
+++ mongo-sharding-repl/init.sh
@@ -13,24 +13,28 @@ rs.initiate(
 exit();
 EOF
 
-docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard1-1 mongosh --port 27022 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard1",
       members: [
-        { _id : 0, host : "shard1:27018" }
+        { _id : 0, host : "shard1-1:27022" },
+        { _id : 1, host : "shard1-2:27023" },
+        { _id : 2, host : "shard1-3:27024" }
       ]
     }
 );
 exit();
 EOF
 
-docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+docker compose exec -T shard2-1 mongosh --port 27025 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 1, host : "shard2:27019" }
+        { _id : 3, host : "shard2-1:27025" },
+        { _id : 4, host : "shard2-2:27026" },
+        { _id : 5, host : "shard2-3:27027" }
       ]
     }
   );
@@ -39,8 +43,8 @@ EOF
 
 docker compose exec -T router1 mongosh --port 27020 --quiet <<EOF
 
-sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "shard1/shard1-1:27022");
+sh.addShard( "shard2/shard2-1:27025");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
@@ -49,8 +53,8 @@ EOF
 
 docker compose exec -T router2 mongosh --port 27021 --quiet <<EOF
 
-sh.addShard( "shard1/shard1:27018");
-sh.addShard( "shard2/shard2:27019");
+sh.addShard( "shard1/shard1-1:27022");
+sh.addShard( "shard2/shard2-1:27025");
 
 sh.enableSharding("somedb");
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
@@ -63,13 +67,37 @@ db.helloDoc.countDocuments()
 exit();
 EOF
 
-docker compose exec -T shard1 mongosh --port 27018 --quiet <<EOF
+docker compose exec -T shard1-1 mongosh --port 27022 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 exit();
 EOF
 
-docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+docker compose exec -T shard1-2 mongosh --port 27023 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+docker compose exec -T shard1-3 mongosh --port 27024 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+docker compose exec -T shard2-1 mongosh --port 27025 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+docker compose exec -T shard2-2 mongosh --port 27026 --quiet <<EOF
+use somedb
+db.helloDoc.countDocuments()
+exit();
+EOF
+
+docker compose exec -T shard2-3 mongosh --port 27027 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 exit();
